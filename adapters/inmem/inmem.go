02a70208db32705916fcd9bfc225c70026d0c478
m-kostrzewa <kostrzewamc@gmail.com>
2018-06-17 13:48:50 +0200

    Mutex in inmem repository.

--- adapters/inmem/inmem.go
+++ adapters/inmem/inmem.go
@@ -1,8 +1,13 @@
 package inmem
 
-import "github.com/m-kostrzewa/powershell-for-programmers/core/question"
+import (
+	"sync"
+
+	"github.com/m-kostrzewa/powershell-for-programmers/core/question"
+)
 
 type InMemQuestionRepository struct {
+	mutex     sync.Mutex
 	questions map[question.QuestionID]*question.Question
 }
 
@@ -13,6 +18,8 @@ func New() *InMemQuestionRepository {
 }
 
 func (r *InMemQuestionRepository) Find(guid question.QuestionID) (*question.Question, error) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
 	if val, ok := r.questions[guid]; ok {
 		return val, nil
 	} else {
@@ -21,6 +28,8 @@ func (r *InMemQuestionRepository) Find(guid question.QuestionID) (*question.Ques
 }
 
 func (r *InMemQuestionRepository) FindAll() []*question.Question {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
 	list := make([]*question.Question, 0)
 	for _, val := range r.questions {
 		list = append(list, val)
@@ -29,6 +38,8 @@ func (r *InMemQuestionRepository) FindAll() []*question.Question {
 }
 
 func (r *InMemQuestionRepository) Store(q *question.Question) error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
 	r.questions[q.QuestionID] = q
 	return nil
 }
